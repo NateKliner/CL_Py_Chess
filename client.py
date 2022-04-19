9ae87fadcbe2cf1e70bdbe967da85ee9cffac606
Nate Kline <55374060+NateKliner@users.noreply.github.com>
2022-04-18 20:58:56 -0700

    half-implemented chess
    We are now able to play a chess game to checkmate, but only from the client side. Next step is to make it so that we can send messages from the server side and play a proper game between client and server.

--- client.py
+++ client.py
@@ -2,26 +2,39 @@ import socket
 import time
 import sys
 import select
+import chess
 
-ip = (socket.gethostname(), 1241)
+ip = (socket.gethostname(), 1244)
 #other = "10.12.112.173"
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(ip)
-
-#for line in sys.stdin:
-#conn.send(line, ip)
-#sent = False
+boardTemp = chess.Board()
+print("---Board Layout---")
+print("8|")
+print("7|")
+print("6|")
+print("5|")
+print("4|")
+print("3|")
+print("2|")
+print("1|")
+print("  ---------------")
+print("  a b c d e f g h\n")
+print("Game Start, you are upper-case!\n")
+print(boardTemp)
 
 while True:
     #msg = "hello sir"
     try:
         for msg in sys.stdin:
-            s.sendto(msg, ip)
+            s.sendto(msg.encode(), ip)
             break
         print("waiting")
         data, server = s.recvfrom(1024)
-        print("recieved: " + data)
+        board = chess.Board(data.decode())
+        print(board)
+        print("_______________NEXT BOARD_______________")
         
-    except:
-        print("error")
+    except Exception as e:
+        print(e)
     time.sleep(1)
